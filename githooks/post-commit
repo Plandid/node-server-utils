244be3647bfd2ddc56b6b4a966a84c1095f12d44
carlschader <carlschader@gmail.com>
2021-07-05 12:34:17 -0600

    updated nod-scripts

--- githooks/post-commit
+++ githooks/post-commit
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 const process = require('process');
@@ -9,14 +9,4 @@ var packageJson = JSON.parse(
     fs.readFileSync(path.join(process.cwd(), 'package.json'))
 );
 
-exec('git tag ' + packageJson.version, (error, stderr, stdout) => {
-    if (error) {
-        console.error(error);
-        process.exit(1);
-    }
-    if (stderr) {
-        console.error(stderr);
-        process.exit(1);
-    }
-    console.log(stdout);
-});
+execSync('git tag ' + packageJson.version);
---
+++ src/index.js
@@ -0,0 +1,111 @@
+import axios from 'axios';
+
+/* module variables */
+
+let jwtKeys = {};
+let serviceIdMap = {};
+
+/* helper functions */
+
+function useFilter(req, pathFilter, recordFilter) {
+    let filter = {};
+    let record = {};
+
+    for (const key in req.params) {
+        filter[key] = pathFilter.hasOwnProperty(key) ? pathFilter[key](req.params[key]) : req.params[key];
+    }
+
+    for (const key in req.query) {
+        record[key] = recordFilter.hasOwnProperty(key) ? recordFilter[key](req.query[key]) : req.query[key];
+    }
+
+    for (const key in req.body) {
+        record[key] = recordFilter.hasOwnProperty(key) ? recordFilter[key](req.body[key]) : req.body[key];
+    }
+
+    return {filter: filter, record: record};
+}
+
+async function getService() {
+    let variables = {};
+    try {
+        const res = await axios.get(new URL(`services/${process.env.SERVICE_ID}`, process.env.APPDATA_DRIVER_URL).href, {
+            headers: {Authorization: `Basic ${getPlandidAuthToken()}`}
+        });
+        variables = res.data;
+    } catch (error) {
+        console.error("couldn't fetch environment");
+        console.error(error);
+    }
+    return variables ? variables : {};
+}
+
+async function getClients() {
+    let variables = {};
+    try {
+        const res = await axios.get(new URL(`clients`, process.env.APPDATA_DRIVER_URL).href, {
+            headers: {Authorization: `Basic ${getPlandidAuthToken()}`}
+        });
+        variables = res.data;
+    } catch (error) {
+        console.error("couldn't fetch environment");
+        console.error(error);
+    }
+    return variables ? variables : {};
+}
+
+async function getEnvironment() {
+    let variables = {};
+    try {
+        const res = await axios.get(new URL(`services/${process.env.SERVICE_ID}`, process.env.APPDATA_DRIVER_URL).href, {
+            headers: {Authorization: `Basic ${getPlandidAuthToken()}`}
+        });
+        variables = res.data.environmentVariables;
+    } catch (error) {
+        console.error("couldn't fetch environment");
+        console.error(error);
+    }
+    return variables ? variables : {};
+}
+
+async function updateJwtKeys() {
+    const service = await getService();
+    const clients = await getClients();
+    let newKeys = {};
+    
+    for (const client of clients) {
+        if (service.supportedClients.hasOwnProperty(client.name)) {
+            jwtKeys[client.name] = client.jwtKey;
+        }
+    }
+
+    jwtKeys = newKeys;
+}
+
+async function updateServiceIdMap() {
+    let newMap = {};
+    const res = await axios.get(new URL("services", process.env.APPDATA_DRIVER_URL).href, {
+        headers: {Authorization: `Basic ${getPlandidAuthToken()}`}
+    });
+    
+    for (const service of res.data) {
+        newMap[service.name] = service._id;
+    }
+
+    serviceIdMap = newMap;
+}
+
+/* exported functions */
+
+export function checkServiceCreds(serviceName, serviceId) {
+    if (serviceIdMap.hasOwnProperty(serviceName) && serviceIdMap[serviceName] === serviceId) {
+        return true;
+    } else {
+        await updateServiceIdMap();
+        return serviceIdMap.hasOwnProperty(serviceName) && serviceIdMap[serviceName] === serviceId;
+    }
+}
+
+export function createAuthToken(username, password) {
+    return Buffer.from(`${username}:${password}`, 'utf8').toString('base64');
+}
